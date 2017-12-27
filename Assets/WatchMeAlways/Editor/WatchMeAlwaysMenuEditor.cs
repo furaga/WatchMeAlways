9dd6d8417c64f74565d9939a3fb5a874c2f8fb0f
DESKTOP-5J7ES5Q\furaga <furaga.fukahori@gmail.com>
2017-12-27 22:37:17 +0900

    Create menu item: WatchMeAlways/GameObject/VideoRecorder

---
+++ Assets/WatchMeAlways/Editor/WatchMeAlwaysMenuEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace WatchMeAlways
+{
+    public class WatchMeAlwaysMenuEditor : MonoBehaviour
+    {
+        [MenuItem("WatchMeAlways/GameObject/VideoRecorder", false, 10)]
+        private static void CreateVideoRecorderObject(MenuCommand menuCommand)
+        {
+            GameObject videoRecorderPrefab = PrefabUtility.InstantiatePrefab(Resources.Load("Prefabs/VideoRecorder")) as GameObject;
+            videoRecorderPrefab.name = "VideoRecorder";
+            PrefabUtility.DisconnectPrefabInstance(videoRecorderPrefab);
+            GameObjectUtility.SetParentAndAlign(videoRecorderPrefab, menuCommand.context as GameObject);
+            Undo.RegisterCreatedObjectUndo(videoRecorderPrefab, "Create " + videoRecorderPrefab.name);
+            Selection.activeObject = videoRecorderPrefab;
+        }
+    }
+}
