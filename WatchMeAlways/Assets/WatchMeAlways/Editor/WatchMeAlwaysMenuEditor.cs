2ff4bb26a35b566d59943bd8d379d8edc10e902b
DESKTOP-5J7ES5Q\furaga <furaga.fukahori@gmail.com>
2018-02-08 18:13:32 +0900

    Create Menu

--- WatchMeAlways/Assets/WatchMeAlways/Editor/WatchMeAlwaysMenuEditor.cs
+++ WatchMeAlways/Assets/WatchMeAlways/Editor/WatchMeAlwaysMenuEditor.cs
@@ -10,21 +10,125 @@ namespace WatchMeAlways
 {
     public class WatchMeAlwaysMenuEditor : MonoBehaviour
     {
-        //[DllImport("WatchMeAlwaysLib")]
-        //static extern float FooPluginFunction();
+        static bool enableInstantReplay_ = false;
+        static bool recording_ = false;
 
-        //[MenuItem("WatchMeAlways/GameObject/VideoRecorder", false, 10)]
-        //private static void CreateVideoRecorderObject(MenuCommand menuCommand)
-        //{
-        //    var res = FooPluginFunction();
-        //    Debug.Log("FooPluginFunctino() = " + res);
+        // SaveDir is fullpath
+        static string SaveDir
+        {
+            get
+            {
+                string documentRoot = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string path = System.IO.Path.Combine(documentRoot, "WatchMeAlways");
+                path = System.IO.Path.Combine(path, "Gallery");
+                return path;
+            }
+        }
 
-        //    //GameObject videoRecorderPrefab = PrefabUtility.InstantiatePrefab(Resources.Load("Prefabs/VideoRecorder")) as GameObject;
-        //    //videoRecorderPrefab.name = "VideoRecorder";
-        //    //PrefabUtility.DisconnectPrefabInstance(videoRecorderPrefab);
-        //    //GameObjectUtility.SetParentAndAlign(videoRecorderPrefab, menuCommand.context as GameObject);
-        //    //Undo.RegisterCreatedObjectUndo(videoRecorderPrefab, "Create " + videoRecorderPrefab.name);
-        //    //Selection.activeObject = videoRecorderPrefab;
-        //}
+        // createDirectoryIfNotExists returns if the directory was created or not.
+        static bool createDirectoryIfNotExists(string path)
+        {
+            if (System.IO.Directory.Exists(path))
+            {
+                return false;
+            }
+            System.IO.Directory.CreateDirectory(path);
+            return true;
+        }
+
+        //
+        // Instant Replay
+        //
+
+        [MenuItem("WatchMeAlways/Save Instant Replay %F10", false, 10)]
+        private static void SaveInstantReplay()
+        {
+            Debug.Log("SaveInstantReplay");
+        }
+
+        [MenuItem("WatchMeAlways/Save Instant Replay %F10", true, 10)]
+        private static bool ValidateSaveInstantReplay()
+        {
+            return enableInstantReplay_;
+        }
+
+        [MenuItem("WatchMeAlways/Enable Instant Replay", false, 10)]
+        private static void EnableInstantReplay()
+        {
+            enableInstantReplay_ = true;
+            Debug.Log("Instant Replay: ON");
+        }
+
+        [MenuItem("WatchMeAlways/Enable Instant Replay", true)]
+        private static bool ValidateEnableInstantReplay()
+        {
+            return !enableInstantReplay_;
+        }
+
+        [MenuItem("WatchMeAlways/Disable Instant Replay", false, 10)]
+        private static void DisableInstantReplay()
+        {
+            enableInstantReplay_ = false;
+            Debug.Log("Instant Replay: OFF");
+        }
+
+        [MenuItem("WatchMeAlways/Disable Instant Replay", true)]
+        private static bool ValidateDisableInstantReplay()
+        {
+            return enableInstantReplay_;
+        }
+
+        //
+        // Recording
+        //
+
+        [MenuItem("WatchMeAlways/Start Recording %F9", false, 50)]
+        private static void StartRecording()
+        {
+            recording_ = true;
+            Debug.Log("Recording: STARTED");
+        }
+
+        [MenuItem("WatchMeAlways/Start Recording %F9", true)]
+        private static bool ValidateStartRecording()
+        {
+            return !recording_;
+        }
+
+        [MenuItem("WatchMeAlways/Finish Recording %F9", false, 50)]
+        private static void FinishRecording()
+        {
+            recording_ = false;
+            Debug.Log("Recording: FINISHED. The recorded video was saved in " + SaveDir);
+        }
+
+        [MenuItem("WatchMeAlways/Finish Recording %F9", true)]
+        private static bool ValidateFinishRecording()
+        {
+            return recording_;
+        }
+
+        //
+        // Screenshot
+        //
+
+        [MenuItem("WatchMeAlways/Take Screenshot %F1", false, 70)]
+        private static void TakeScreenshot()
+        {
+            Debug.Log("Screenshot: a new screenshot was saved in " + SaveDir);
+        }
+
+        [MenuItem("WatchMeAlways/Open Gallery in Explorer", false, 90)]
+        private static void OpenGalleryInExplorer()
+        {
+            createDirectoryIfNotExists(SaveDir);
+            System.Diagnostics.Process.Start(SaveDir);
+            Debug.Log("Open gallery folder " + SaveDir + " in Explorer");
+        }
+
+        [MenuItem("WatchMeAlways/Config", false, 110)]
+        private static void Config()
+        {
+        }
     }
 }
