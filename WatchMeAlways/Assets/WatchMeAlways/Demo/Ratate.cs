a3b1ff48b87c826b560fed750b9918d49daf5a5c
DESKTOP-5J7ES5Q\furaga <furaga.fukahori@gmail.com>
2018-02-08 16:50:24 +0900

    Demo1: Rotate box

---
+++ WatchMeAlways/Assets/WatchMeAlways/Demo/Ratate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ratate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dt = Time.deltaTime;
+        float wx = 360 * dt * 0.5f;
+        float wy = 360 * dt * 0.3f;
+        float wz = 360 * dt * 0.2f;
+        transform.Rotate(new Vector3(wx, wy, wz));
+	}
+}
