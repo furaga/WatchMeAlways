a53e9dc0135a10c112e4d2f34c1b1af0ffc15ffa
DESKTOP-5J7ES5Q\furaga <furaga.fukahori@gmail.com>
2018-02-16 19:49:37 +0900

    Add script to show elapsed time

---
+++ WatchMeAlways/Assets/WatchMeAlways/Demo/PrintTime.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WatchMeAlways
+{
+    public class PrintTime : MonoBehaviour
+    {
+        System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+
+        // Use this for initialization
+        void Start()
+        {
+            stopWatch.Start();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnGUI()
+        {
+            GUI.Label(new Rect(20, 20, 300, 40), string.Format("経過時間: {0:   0.00}秒", stopWatch.Elapsed.TotalSeconds));
+        }
+    }
+}
