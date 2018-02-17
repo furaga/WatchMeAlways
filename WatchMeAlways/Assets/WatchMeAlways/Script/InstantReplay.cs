cceb0caf7f3851671b4a840104b73419048c5136
DESKTOP-5J7ES5Q\furaga <furaga.fukahori@gmail.com>
2018-02-18 02:08:38 +0900

    WIP

---
+++ WatchMeAlways/Assets/WatchMeAlways/Script/InstantReplay.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace WatchMeAlways
+{
+    public class InstantReplay : Singleton<InstantReplay>
+    {
+        void Start()
+        {
+
+        }
+
+        void Stop()
+        {
+
+        }
+
+        void Save()
+        {
+
+        }
+        void GetSetting()
+        {
+
+        }
+
+        void ApplySetting()
+        {
+
+        }
+
+        void IsRecording()
+        {
+
+        }
+
+        void GetMonitors()
+        {
+
+        }
+
+        void loadSetting()
+        {
+
+        }
+
+        void run()
+        {
+
+        }
+
+        void kill()
+        {
+
+        }
+
+        void sendMessage(string msg)
+        {
+
+        }
+
+    }
+}
